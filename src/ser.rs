d6671653263682a9c3e935137c0c8a70cd5afa8b
Tim Yuen <timothy.yuen22@gmail.com>
2023-07-10 21:50:12 -0400

    initial serialize impl, need to refactor

--- src/ser.rs
+++ src/ser.rs
@@ -2,10 +2,21 @@ use serde::{ser, Serialize};
 
 use crate::error::{Error, Result};
 
+// TODO might need to account for indentation depth
+const OPEN_CURLY: &str = " {\n";
+const CLOSE_CURLY: &str = "\n}";
+const OPEN_SQUARE: &str = " [\n";
+const CLOSE_SQUARE: &str = "\n]";
+
 #[derive(Default)]
 pub struct Serializer {
+    /// The working string that things are serialized into.
     output: String,
+    /// The number of indents. Used for determining how to format closing brackets.
+    indents: u32,
 }
+
+impl Serializer {}
 
 impl<'a> ser::Serializer for &'a mut Serializer {
     type Ok = ();
@@ -27,168 +38,221 @@ impl<'a> ser::Serializer for &'a mut Serializer {
     type SerializeStructVariant = Self;
 
     fn serialize_bool(self, v: bool) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        self.output += if v { "true" } else { "false" };
+        Ok(())
     }
 
     fn serialize_i8(self, v: i8) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        self.serialize_i64(v.into())
     }
 
     fn serialize_i16(self, v: i16) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        self.serialize_i64(v.into())
     }
 
     fn serialize_i32(self, v: i32) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        self.serialize_i64(v.into())
     }
 
+    // TODO the serde tutorial mentions that using the itoa crate will be faster
     fn serialize_i64(self, v: i64) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        self.output += &v.to_string();
+        Ok(())
     }
 
     fn serialize_u8(self, v: u8) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        self.serialize_u64(v.into())
     }
 
     fn serialize_u16(self, v: u16) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        self.serialize_u64(v.into())
     }
 
     fn serialize_u32(self, v: u32) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        self.serialize_u64(v.into())
     }
 
+    // TODO the serde tutorial mentions that using the itoa crate will be faster
     fn serialize_u64(self, v: u64) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        self.output += &v.to_string();
+        Ok(())
     }
 
     fn serialize_f32(self, v: f32) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        self.serialize_f64(v.into())
     }
 
+    // TODO the serde tutorial mentions that using the itoa crate will be faster
     fn serialize_f64(self, v: f64) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        self.output += &v.to_string();
+        Ok(())
     }
 
     fn serialize_char(self, v: char) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        self.serialize_str(&v.to_string())
     }
 
+    // TODO handle strings with quotes
     fn serialize_str(self, v: &str) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        self.output += "\"";
+        self.output += v;
+        self.output += "\"";
+
+        Ok(())
     }
 
     fn serialize_bytes(self, v: &[u8]) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        use serde::ser::SerializeSeq;
+
+        let mut seq = self.serialize_seq(Some(v.len()))?;
+        for byte in v {
+            seq.serialize_element(byte)?;
+        }
+
+        seq.end()
     }
 
     fn serialize_none(self) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        self.serialize_unit()
     }
 
     fn serialize_some<T: ?Sized>(self, value: &T) -> std::result::Result<Self::Ok, Self::Error>
     where
         T: Serialize,
     {
-        todo!()
+        value.serialize(self)
     }
 
     fn serialize_unit(self) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        self.output += "()";
+        Ok(())
     }
 
-    fn serialize_unit_struct(
-        self,
-        name: &'static str,
-    ) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+    fn serialize_unit_struct(self, _: &'static str) -> std::result::Result<Self::Ok, Self::Error> {
+        self.serialize_unit()
     }
 
     fn serialize_unit_variant(
         self,
-        name: &'static str,
-        variant_index: u32,
+        _name: &'static str,
+        _variant_index: u32,
         variant: &'static str,
     ) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        self.serialize_str(variant)
     }
 
     fn serialize_newtype_struct<T: ?Sized>(
         self,
-        name: &'static str,
+        _name: &'static str,
         value: &T,
     ) -> std::result::Result<Self::Ok, Self::Error>
     where
         T: Serialize,
     {
-        todo!()
+        value.serialize(self)
     }
 
     fn serialize_newtype_variant<T: ?Sized>(
         self,
-        name: &'static str,
-        variant_index: u32,
+        _name: &'static str,
+        _variant_index: u32,
         variant: &'static str,
         value: &T,
     ) -> std::result::Result<Self::Ok, Self::Error>
     where
         T: Serialize,
     {
-        todo!()
+        // self.output += OPEN_CURLY;
+        // self.indents += 1;
+        bracket(self, Bracket::OpenCurly);
+        variant.serialize(&mut *self)?;
+        self.output += " ";
+        value.serialize(&mut *self)?;
+        // self.indents -= 1;
+        // self.output += CLOSE_CURLY;
+        bracket(self, Bracket::CloseCurly);
+
+        Ok(())
     }
 
     fn serialize_seq(
         self,
-        len: Option<usize>,
+        _len: Option<usize>,
     ) -> std::result::Result<Self::SerializeSeq, Self::Error> {
-        todo!()
+        // self.output += OPEN_SQUARE;
+        // self.indents += 1;
+        bracket(self, Bracket::OpenSquare);
+
+        Ok(self)
     }
 
     fn serialize_tuple(self, len: usize) -> std::result::Result<Self::SerializeTuple, Self::Error> {
-        todo!()
+        self.serialize_seq(Some(len))
     }
 
     fn serialize_tuple_struct(
         self,
-        name: &'static str,
+        _name: &'static str,
         len: usize,
     ) -> std::result::Result<Self::SerializeTupleStruct, Self::Error> {
-        todo!()
+        self.serialize_seq(Some(len))
     }
 
     fn serialize_tuple_variant(
         self,
-        name: &'static str,
-        variant_index: u32,
+        _name: &'static str,
+        _variant_index: u32,
         variant: &'static str,
-        len: usize,
+        _len: usize,
     ) -> std::result::Result<Self::SerializeTupleVariant, Self::Error> {
-        todo!()
+        // self.output += OPEN_CURLY;
+        // self.indents += 1;
+        bracket(self, Bracket::OpenCurly);
+        variant.serialize(&mut *self)?;
+        // self.output += OPEN_SQUARE;
+        // self.indents += 1;
+        bracket(self, Bracket::OpenSquare);
+
+        Ok(self)
     }
 
     fn serialize_map(
         self,
-        len: Option<usize>,
+        _len: Option<usize>,
     ) -> std::result::Result<Self::SerializeMap, Self::Error> {
-        todo!()
+        // if self.indents > 0 {
+        //     self.output += OPEN_CURLY;
+        // }
+        // self.indents += 1;
+        bracket(self, Bracket::OpenCurly);
+
+        Ok(self)
     }
 
     fn serialize_struct(
         self,
-        name: &'static str,
+        _name: &'static str,
         len: usize,
     ) -> std::result::Result<Self::SerializeStruct, Self::Error> {
-        todo!()
+        self.serialize_map(Some(len))
     }
 
     fn serialize_struct_variant(
         self,
-        name: &'static str,
-        variant_index: u32,
+        _name: &'static str,
+        _variant_index: u32,
         variant: &'static str,
-        len: usize,
+        _len: usize,
     ) -> std::result::Result<Self::SerializeStructVariant, Self::Error> {
-        todo!()
+        // self.output += OPEN_CURLY;
+        // self.indents += 1;
+        bracket(self, Bracket::OpenCurly);
+        variant.serialize(&mut *self)?;
+        // self.output += OPEN_CURLY;
+        // self.indents += 1;
+        bracket(self, Bracket::OpenCurly);
+
+        Ok(self)
     }
 }
 
@@ -201,11 +265,16 @@ impl<'a> ser::SerializeSeq for &'a mut Serializer {
     where
         T: Serialize,
     {
-        todo!()
+        self.output += " ";
+        value.serialize(&mut **self)
     }
 
     fn end(self) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        // self.indents -= 1;
+        // self.output += CLOSE_SQUARE;
+        bracket(self, Bracket::CloseSquare);
+
+        Ok(())
     }
 }
 
@@ -218,11 +287,16 @@ impl<'a> ser::SerializeTuple for &'a mut Serializer {
     where
         T: Serialize,
     {
-        todo!()
+        self.output += " ";
+        value.serialize(&mut **self)
     }
 
     fn end(self) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        // self.indents -= 1;
+        // self.output += CLOSE_SQUARE;
+        bracket(self, Bracket::CloseSquare);
+
+        Ok(())
     }
 }
 
@@ -235,11 +309,16 @@ impl<'a> ser::SerializeTupleStruct for &'a mut Serializer {
     where
         T: Serialize,
     {
-        todo!()
+        self.output += " ";
+        value.serialize(&mut **self)
     }
 
     fn end(self) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        // self.indents -= 1;
+        // self.output += CLOSE_SQUARE;
+        bracket(self, Bracket::CloseSquare);
+
+        Ok(())
     }
 }
 
@@ -252,14 +331,23 @@ impl<'a> ser::SerializeTupleVariant for &'a mut Serializer {
     where
         T: Serialize,
     {
-        todo!()
+        self.output += " ";
+        value.serialize(&mut **self)
     }
 
     fn end(self) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        // self.indents -= 1;
+        // self.output += CLOSE_SQUARE;
+        // self.indents -= 1;
+        // self.output += CLOSE_CURLY;
+        bracket(self, Bracket::CloseSquare);
+        bracket(self, Bracket::CloseCurly);
+
+        Ok(())
     }
 }
 
+// TODO need to use custom object so we can write keys as literal strings
 impl<'a> ser::SerializeMap for &'a mut Serializer {
     type Ok = ();
     type Error = Error;
@@ -268,18 +356,26 @@ impl<'a> ser::SerializeMap for &'a mut Serializer {
     where
         T: Serialize,
     {
-        todo!()
+        self.output += " ";
+        key.serialize(&mut **self)
     }
 
     fn serialize_value<T: ?Sized>(&mut self, value: &T) -> std::result::Result<(), Self::Error>
     where
         T: Serialize,
     {
-        todo!()
+        self.output += " ";
+        value.serialize(&mut **self)
     }
 
     fn end(self) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        // self.indents -= 1;
+        // if self.indents > 0 {
+        //     self.output += CLOSE_CURLY;
+        // }
+        bracket(self, Bracket::CloseCurly);
+
+        Ok(())
     }
 }
 
@@ -295,11 +391,20 @@ impl<'a> ser::SerializeStruct for &'a mut Serializer {
     where
         T: Serialize,
     {
-        todo!()
+        self.output += " ";
+        key.serialize(&mut **self)?;
+        self.output += " ";
+        value.serialize(&mut **self)
     }
 
     fn end(self) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        // self.indents -= 1;
+        // if self.indents > 0 {
+        //     self.output += CLOSE_CURLY;
+        // }
+        bracket(self, Bracket::CloseCurly);
+
+        Ok(())
     }
 }
 
@@ -315,11 +420,56 @@ impl<'a> ser::SerializeStructVariant for &'a mut Serializer {
     where
         T: Serialize,
     {
-        todo!()
+        self.output += " ";
+        key.serialize(&mut **self)?;
+        self.output += " ";
+        value.serialize(&mut **self)
     }
 
     fn end(self) -> std::result::Result<Self::Ok, Self::Error> {
-        todo!()
+        bracket(self, Bracket::CloseCurly);
+        // self.indents -= 1;
+        // self.output += CLOSE_CURLY;
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum Bracket {
+    OpenCurly,
+    CloseCurly,
+
+    OpenSquare,
+    CloseSquare,
+}
+
+fn bracket(s: &mut Serializer, b: Bracket) {
+    match b {
+        Bracket::OpenCurly => {
+            if s.indents > 0 {
+                s.output += OPEN_CURLY;
+            }
+            s.indents += 1;
+        }
+        Bracket::CloseCurly => {
+            s.indents -= 1;
+            if s.indents > 0 {
+                s.output += CLOSE_CURLY;
+            }
+        }
+        Bracket::OpenSquare => {
+            if s.indents > 0 {
+                s.output += OPEN_SQUARE
+            }
+            s.indents += 1;
+        }
+        Bracket::CloseSquare => {
+            s.indents -= 1;
+            if s.indents > 0 {
+                s.output += CLOSE_SQUARE;
+            }
+        }
     }
 }
 
@@ -329,4 +479,33 @@ pub fn to_string<T: Serialize>(value: &T) -> Result<String> {
     value.serialize(&mut serializer)?;
 
     Ok(serializer.output)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_struct() {
+        #[derive(serde::Serialize)]
+        struct Inner {
+            num: f64,
+        }
+
+        #[derive(serde::Serialize)]
+        struct TestStruct {
+            boolean: bool,
+            number: f64,
+            string: String,
+            inner: Inner,
+        }
+
+        let test_struct = TestStruct {
+            boolean: true,
+            number: 10.0,
+            string: "hello world!".to_string(),
+            inner: Inner { num: 10.1 },
+        };
+        let output = super::to_string(&test_struct).unwrap();
+
+        assert_eq!(output, "asdf");
+    }
 }
