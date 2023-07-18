22a12a46affa868649ba1c0cb6957a53dcfe692d
Tim Yuen <timothy.yuen22@gmail.com>
2023-07-18 19:46:54 -0400

    add short crate doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+/*!
+A Rust implementation of [Tot](https://github.com/totlang/tot).
+
+Tot is a configuration language meant to be edited by hand.
+
+## Features
+
+* Whitespace-based format that _does not_ require indentation
+* Simple, limited syntax
+* JSON-style objects and lists
+* Reference values (WIP)
+* File import (WIP)
+* Non-Turing complete Lisp-style expressions (WIP)
+* Compatible with:
+    * JSON
+    * YAML
+    * TOML
+
+*/
+
 pub mod de;
 pub use de::from_str;
 pub mod ser;
